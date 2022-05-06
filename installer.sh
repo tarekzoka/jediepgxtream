c6d57dc2a3039fc8e1d97d61817a9778664a8a17
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 01:04:16 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo
 opkg install --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.ipk?raw=true
 wait
 echo
-dpkg -i --force-overwrite /tmp/*.deb https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb?raw=true
+dpkg -i --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb?raw=true
 echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
 wait
