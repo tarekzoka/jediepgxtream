6a05455d706071bc3c9105f3aac04f68dcc964cf
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 00:44:16 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ echo
 opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
 wait
 echo
-dpkg -i --force-overwrite /tmp/*.deb https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+dpkg -i --force-overwrite /tmp/*.deb https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb?raw=true
 echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
 wait
