862c1e54ed72eda3e25c45b0505f71268ea0a9f9
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 00:46:24 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ######################################################################################
-## Command=wget https://raw.githubusercontent.com/tar1971/feeds-finder/main/installer.sh -O - | /bin/sh
+## Command=wget https://raw.githubusercontent.com/tarekzoka/jediepgxtream/main/installer.sh -O - | /bin/sh
 ##
 ###########################################
 ###########################################
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+opkg install --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.ipk?raw=true
 wait
 echo
 dpkg -i --force-overwrite /tmp/*.deb https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb?raw=true
