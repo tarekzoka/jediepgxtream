f9a9123e19b5676f7644900cf377c1fccec98c23
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 00:41:12 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,104 +1,16 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/feeds-finder/main/installer.sh -O - | /bin/sh
+##
+###########################################
+###########################################
 #!/bin/sh
+echo
 
-# ==============================================
-# SCRIPT : DOWNLOAD AND INSTALL JediMakerXtream #
-# =====================================================================================================================
-# Command: wget https://raw.githubusercontent.com/tarekzoka/JediMakerXtream/main/installer.sh -O - | /bin/sh #
-# =====================================================================================================================
-
-########################################################################################################################
-# Plugin	... Enter Manually
-########################################################################################################################
-
-PACKAGE_DIR='JediMakerXtream/main'
-MY_IPK="enigma2-plugin-extensions-jedimakerxtream_6.18_all.ipk"
-MY_DEB="enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb"
-
-
-########################################################################################################################
-# Auto ... Do not change
-########################################################################################################################
-
-# Decide : which package ?
-MY_MAIN_URL="https://raw.githubusercontent.com/tarekzoka/"
-if which dpkg > /dev/null 2>&1; then
-	MY_FILE=$MY_DEB
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
-else
-	MY_FILE=$MY_IPK
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
-fi
-MY_TMP_FILE="/tmp/"$MY_FILE
-
-echo ''
-echo '************************************************************'
-echo '**                         STARTED                        **'
-echo '************************************************************'
-echo "**                 Uploaded by: tarekzoka                   **"
-echo "**  https://www.tunisia-sat.com/forums/threads/3898738/   **"
-echo "************************************************************"
-echo ''
-
-# Remove previous file (if any)
-rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-# Download package file
-MY_SEP='============================================================='
-echo $MY_SEP
-echo 'Downloading '$MY_FILE' ...'
-echo $MY_SEP
-echo ''
-wget -T 2 $MY_URL -P "/tmp/"
-
-# Check download
-if [ -f $MY_TMP_FILE ]; then
-	# Install
-	echo ''
-	echo $MY_SEP
-	echo 'Installation started'
-	echo $MY_SEP
-	echo ''
-	if which dpkg > /dev/null 2>&1; then
-		apt-get install --reinstall $MY_TMP_FILE -y
-	else
-		opkg install --force-reinstall $MY_TMP_FILE
-	fi
-	MY_RESULT=$?
-
-	# Result
-	echo ''
-	echo ''
-	if [ $MY_RESULT -eq 0 ]; then
-		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
-		echo ''
-		echo "   >>>>         RESTARING         <<<<"
-		if which systemctl > /dev/null 2>&1; then
-			sleep 2; systemctl restart enigma2
-		else
-			init 4; sleep 4; init 3;
-		fi
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi;
-	echo ''
-	echo '**************************************************'
-	echo '**                   FINISHED                   **'
-	echo '**************************************************'
-	echo ''
-	exit 0
-else
-	echo ''
-	echo "Download failed !"
-	exit 1
-fi
-	echo '**                   FINISHED                   **'
-	echo '**************************************************'
-	echo ''
-	exit 0
-else
-	echo ''
-	echo "Download failed !"
-	exit 1
-fi
-
-# 
+opkg install --force-overwrite  https://github.com/tar1971/feeds-finder/blob/main/feeds-finder_V1.6_py2_all.ipk?raw=true
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+reboot
+sleep 2;
+exit 0
