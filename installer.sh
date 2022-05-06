aac744b8b6740caccc0ee73fe207987ab4d5b9e4
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 01:09:34 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,6 +11,7 @@ opkg install --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/
 wait
 echo
 dpkg -i --force-overwrite  https://github.com/tarekzoka/jediepgxtream/blob/main/enigma2-plugin-extensions-jedimakerxtream_6.18_all.deb?raw=true
+apt-get update && dpkg -i --force-overwrite /tmp/*.deb; apt-get install -f -y
 echo "**********************************************************************************"
 wait
 reboot
